8fc1a675067297de752200e799869aa8ab58ffbe
grahambarry <grahambarry@live.ie>
2015-11-21 18:17:48 +0000

    s3 testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+require 'rubygems'
+require 'aws-sdk'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
