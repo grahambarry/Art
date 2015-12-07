e4ec7005509b9fc88de7fa963acc00f9eb5249f4
grahambarry <grahambarry@live.ie>
2015-12-07 00:55:07 +0000

    navfixgrey5

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-config.assets.compile = true
+Rails.application.config.assets.compile = true
